92979c3ea0b90b4788d11969a356412f0fe6cc38
Anand Gokul <anandgokul@anandgokul.local>
2020-02-26 13:36:39 -0800

    Adding plotting

---
+++ scripts/plotGraph_wth_Partitions.py
@@ -0,0 +1,40 @@
+"""
+import yaml
+import networkx as nx
+import metis
+import numpy as np
+
+from lib.utils import load_graph_data
+
+graph_pkl_filename="data/sensor_graph_arterial/adj_mx_arterial.pkl"
+
+sensor_ids, sensor_id_to_ind, adj_mx = load_graph_data(graph_pkl_filename)
+
+G = nx.from_numpy_matrix(adj_mx, parallel_edges=False, create_using=nx.DiGraph)
+
+import matplotlib.pyplot as plt
+
+nx.draw(G)  # networkx draw()
+
+plt.draw()  # pyplot draw()
+"""
+
+import networkx as nx
+import metis
+import pydot
+
+from lib.utils import load_graph_data
+
+graph_pkl_filename="data/sensor_graph_arterial/adj_mx_arterial.pkl"
+
+sensor_ids, sensor_id_to_ind, adj_mx = load_graph_data(graph_pkl_filename)
+
+G = nx.from_numpy_matrix(adj_mx, parallel_edges=False, create_using=nx.DiGraph)
+
+(edgecuts, parts) = metis.part_graph(G, 15)
+
+colors = ['red','blue','green']
+for i, p in enumerate(parts):
+	G.node[i]['color'] = colors[p]
+
+nx.write_dot(G, 'graph.dot')
