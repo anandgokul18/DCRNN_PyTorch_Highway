7cc0c1e7958eecae2a204192724628d36f1744fd
Anand Gokul <anandgokul@anandgokul.local>
2020-02-26 19:33:40 -0800

    Final graph code

--- plotGraph_wth_Partitions.py
+++ plotGraph_wth_Partitions.py
@@ -25,16 +25,29 @@ import pydot
 
 from lib.utils import load_graph_data
 
+from networkx.drawing.nx_agraph import write_dot
+
 graph_pkl_filename="data/sensor_graph_arterial/adj_mx_arterial.pkl"
 
 sensor_ids, sensor_id_to_ind, adj_mx = load_graph_data(graph_pkl_filename)
 
 G = nx.from_numpy_matrix(adj_mx, parallel_edges=False, create_using=nx.DiGraph)
 
-(edgecuts, parts) = metis.part_graph(G, 1)
+print("loaded graph...")
 
-colors = ['red']
+(edgecuts, parts) = metis.part_graph(G, 15)
+
+print("partioned graph...")
+
+colors = ['red','yellow', 'blue', 'black', 'green', 'brown', 'purple', 'gray', 'orange', 'pink', 'cyan', 'maroon', 'coral', 'teal', 'crimson']
 for i, p in enumerate(parts):
-	G.node[i]['color'] = colors[p]
+	try:
+		G.nodes[i]['color'] = colors[p]
+	except:
+		print("Using 'azure' color instead of "+colors[p])
+		G.nodes[i]['color'] = 'azure'
 
-nx.write_dot(G, 'graph.dot')
+
+print("color coded graph...")
+
+write_dot(G, 'graph.dot')
