4b1f9824eca8c6bd3e0c822c8bf33f0709b97d31
Anand Gokul <anandgokul@anandgokul.local>
2020-03-27 03:02:09 -0700

    Bug fixes

--- application.py
+++ application.py
@@ -78,9 +78,10 @@ def predict(config_filename='data/model/dcrnn_highway_flask.yaml', current_cuda_
         if not split_into_subgraphs:
             if not os.path.exists(supervisor_config['data'].get('dataset_dir')):
                 os.makedirs(supervisor_config['data'].get('dataset_dir'))
-            np.savez_compressed(supervisor_config['data'].get('dataset_dir')+'/'+'test.npz', x=sensor_data['x'], y=sensor_data['y'])
-            np.savez_compressed(supervisor_config['data'].get('dataset_dir')+'/'+'train.npz', x=None, y=None)
-            np.savez_compressed(supervisor_config['data'].get('dataset_dir')+'/'+'val.npz', x=None, y=None)
+            np.savez_compressed(supervisor_config['data'].get('dataset_dir')+'/'+'test.npz', x=sensor_data['x'], y=sensor_data['y'], x_offset=None, y_offset=None)
+            #These train and val are not used anywhere, but as assert
+            np.savez_compressed(supervisor_config['data'].get('dataset_dir')+'/'+'train.npz', x=sensor_data['x'], y=sensor_data['y'], x_offset=None, y_offset=None)
+            np.savez_compressed(supervisor_config['data'].get('dataset_dir')+'/'+'val.npz', x=sensor_data['x'], y=sensor_data['y'], x_offset=None, y_offset=None)
 
         #Moving import here since the global variable for cuda device is declared above
         import model.pytorch.dcrnn_supervisor as dcrnn_supervisor
